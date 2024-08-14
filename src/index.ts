41f570f3e1fc5695bf32ede8f1ae312375ef4fe2
gabrielCesarino <gahnrq@hotmail.com>
2024-08-13 21:03:41 -0300

    add exports

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,1 @@
-import { sign } from "./jwt/sign";
-
-sign({
-  exp: new Date().valueOf() + 24 * 60 * 60 * 1000,
-  data: {
-    sub: "2",
-    roles: ["admin", "user"],
-  },
-  secret: "",
-});
+export * from "./jwt";
